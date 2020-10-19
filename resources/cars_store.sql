4b8755b1c0824d7d2ed8ec8a6eba83485d743b1b
Konrad Mihelsson <konrad.mihelsson@gmail.com>
2020-10-19 16:56:36 +0300

    2. Уровень - Джуниор. Блок 2.3. SQL, JDBC. 3. Outer join. Хранилище машин [#302048]

--- resources/cars_store.sql
+++ resources/cars_store.sql
@@ -70,29 +70,14 @@ values ('Rio', 'Kia', 4, 5, 5);
 insert into car (model, manufacturer, body_id, engine_id, transmission_id)
 values ('Alphard', 'Toyota', 5, 6, 7);
 
-select car.model, car.manufacturer, body.model, engine.model, transmission.model
-from car,
-     body,
-     engine,
-     transmission
-where car.body_id = body.id
-  AND car.engine_id = engine.id
-  AND car.transmission_id = transmission.id;
+select *
+from car
+       left join body on car.body_id = body.id
+       left join engine on car.engine_id = engine.id
+       left join transmission on car.transmission_id = transmission.id;
 
-select body.model, body.manufacturer
-from body
-       left join car
-                 on body.id = car.body_id
-where car.id is null
-union
-select engine.model, engine.manufacturer
-from engine
-       left join car
-                 on engine.id = car.body_id
-where car.id is null
-union
-select transmission.model, transmission.manufacturer
-from transmission
-       left join car
-                 on transmission.id = car.body_id
-where car.id is null;
+select *
+from car
+       full join body b on car.body_id = b.id
+       full join engine e on car.engine_id = e.id
+       full join transmission t on car.transmission_id = t.id where car is null;
